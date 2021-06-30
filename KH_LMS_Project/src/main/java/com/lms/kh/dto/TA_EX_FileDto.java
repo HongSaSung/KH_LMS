85fe91b48d697b548836a4eea555ac98da644dd6
ksjj4791 <ksjj4791@gmail.com>
2021-06-30 21:03:39 +0900

    작성자 : 김송주 내용 : DTO 추가 (추후 합쳐야함)

---
+++ KH_LMS_Project/src/main/java/com/lms/kh/dto/TA_EX_FileDto.java
@@ -0,0 +1,10 @@
+package com.lms.kh.dto;
+
+public class TA_EX_FileDto {
+	private String ta_ex_seq;
+	private String ta_ex_part;
+	private int ref_seq;
+	private String ta_ex_name;
+	private String ta_ex_path;
+	private String regdate;
+}
---
+++ KH_LMS_Project/src/main/java/com/lms/kh/dto/UC_LinkDto.java
@@ -0,0 +1,7 @@
+package com.lms.kh.dto;
+
+public class UC_LinkDto {
+	private int uc_link_seq;
+	private String userid;
+	private int co_seq;
+}
