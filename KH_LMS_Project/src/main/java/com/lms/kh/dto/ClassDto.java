95fe1ba0ece7b923bb09fbe8362bc19d5705bf25
ksjj4791 <ksjj4791@gmail.com>
2021-07-01 12:07:36 +0900

    작성자 : 김송주 내용 : 과정 개설 / 과정 리스트 출력 Mapper

--- KH_LMS_Project/src/main/java/com/lms/kh/dto/ClassDto.java
+++ KH_LMS_Project/src/main/java/com/lms/kh/dto/ClassDto.java
@@ -4,4 +4,39 @@ public class ClassDto {
 	private int cl_seq;
 	private String cl_title;
 	private String cl_desc;
+	
+	public ClassDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public ClassDto(int cl_seq, String cl_title, String cl_desc) {
+		super();
+		this.cl_seq = cl_seq;
+		this.cl_title = cl_title;
+		this.cl_desc = cl_desc;
+	}
+	public int getCl_seq() {
+		return cl_seq;
+	}
+	public void setCl_seq(int cl_seq) {
+		this.cl_seq = cl_seq;
+	}
+	public String getCl_title() {
+		return cl_title;
+	}
+	public void setCl_title(String cl_title) {
+		this.cl_title = cl_title;
+	}
+	public String getCl_desc() {
+		return cl_desc;
+	}
+	public void setCl_desc(String cl_desc) {
+		this.cl_desc = cl_desc;
+	}
+	@Override
+	public String toString() {
+		return "ClassDto [cl_seq=" + cl_seq + ", cl_title=" + cl_title + ", cl_desc=" + cl_desc + "]";
+	}
+	
+	
 }
